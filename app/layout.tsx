5d979d58604606a77275c8e4caff4f8d17b93e35
Geovanny-Gomez-Arciniegas <geovanny_gomeza@soy.sena.edu.co>
2024-02-01 04:00:56 -0500

    Se borra los datos anteriores que tenia el layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,24 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <html lang="es">
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
-            <SideNav />
+          <SideNav />
         </div>,
         <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
-            {children}
+          {children}
         </div>
-    </div>
-);
-  // return (
-  //   <html lang="es">
-  //     <div className="w-full flex-none md:w-64">
-  //       <h1> navegacion</h1>
-  //       <SideNav />
-  //     </div>
-  //     <body className={`${inter.className} antialiased`}>{children}</body>
-  //   </html>
-  // );
+      </div>
+    </html>
+  );
 }
 
 
