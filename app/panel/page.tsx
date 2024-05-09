9ee790f64ecf5d31cdbf8404ae8499525b83fcfd
Geovanny-Gomez-Arciniegas <geovanny_gomeza@soy.sena.edu.co>
2024-05-09 17:40:54 -0500

    /pane/page.tsx es cambiado por /panel/layout

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -1,6 +1,6 @@
 import SideNav from "../ui/inicio/sidenav";
 
-export default function Page( { children }: { children: React.ReactNode }) {
+export default function Layout( { children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <nav className="w-full flex-none md:w-64">
