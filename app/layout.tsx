aab7b8438090573ab04b87f687b4bb60b94985b9
Geovanny-Gomez-Arciniegas <geovanny_gomeza@soy.sena.edu.co>
2024-05-09 16:58:47 -0500

    Correccioón de layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   // El '%s' contenido de la plantilla se reemplazará por el título de la página especificada en cada página.,
   title: {
     template: '%s |Alquiler Vacacional',
-    default: 'Alquiler vacacional',
+    default: 'Alquiler Vacacional',
   },
   description: 'The oficial Next.js Course Dashboard, build with App Router',
   metadataBase: new URL('https://acme-dashboard.vercel.app'),
@@ -23,13 +23,12 @@ export default function RootLayout({
     <html className={`${inter.className} antialiased`} lang="es">
       <body>
 
-
         <header>
           <div style={{ objectPosition: "center" }}>
             <Header />
           </div>,
         </header>
-        <main className="flex flex-col p-2 items-center ">
+        <main className="flex flex-col p-2 items-center">
           <div className="flex-row flex w-screen md:flex-row md:overflow-hidden">
             <div className="basis-1/5 w-full flex-none md:w-64">
               <SideNav />
