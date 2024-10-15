ba01839d26831855fa7888e4d3847fb2da5aca4c
Geovanny-Gomez-Arciniegas <geovanny_gomeza@soy.sena.edu.co>
2024-10-14 23:21:28 -0500

    Recuperacion carpeta todavia no se para que sirve carpeta over

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,7 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-import { useDebouncedCallback } from 'use-debounce'; // para evitar que se hagan muchas peticiones a la base de datos con cada tecla que se presiona
+import { useDebouncedCallback } from 'use-debounce'; // se uasara la biblioteca "use-debounce" que se encuentra en la carpetas modulos de node js de para evitar que se hagan muchas peticiones a la base de datos con cada tecla que se presiona
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
